985172c329a76842b2e64c0e03af3b72eb0febef
kim-jos <josephkim828@gmail.com>
2021-07-12 15:50:00 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="app">
+    
+      <Router>
+      <>
+        
+        <Switch>
+          <Route path="/" exact>
+            <h1>This is the homepage</h1> 
+          </Route>
+        </Switch>
+      </>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCRN5l6GPmGP0wb29jRy1lip-y1lFr1yfM",
+  authDomain: "slack-clone-3c577.firebaseapp.com",
+  projectId: "slack-clone-3c577",
+  storageBucket: "slack-clone-3c577.appspot.com",
+  messagingSenderId: "910969077396",
+  appId: "1:910969077396:web:dd2d1d70b9ea687514be81"
+};
