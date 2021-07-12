bda07122a40941032506482c38eb8e2801fb8883
kim-jos <josephkim828@gmail.com>
2021-07-12 20:01:17 +0900

    completed header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import styled from 'styled-components';
 function App() {
   return (
     <div className="app">
-    
       <Router>
-      <>
-        
-        <Switch>
-          <Route path="/" exact>
-            <h1>This is the homepage</h1> 
-          </Route>
-        </Switch>
-      </>
-    </Router>
+        <>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                {/* Chat */}
+              </Route>
+            </Switch>
+
+          </AppBody>
+        </>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+   display: flex; 
+   height: 100vh;
+`
